21050ceeea41bded1e21d9782d2b181fd27cc6d9
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-01 17:30:52 -0300

    fix: delete duplicated font instance

--- src/pages/styles/CandidatePortalStyles.ts
+++ src/pages/styles/CandidatePortalStyles.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    font-family: Radio Canada;
     display: flex;
     flex-direction: column;
     min-height: 100vh;
