17cb195b7f573c85310f458ff2e64aa32d82820a
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-01 17:31:49 -0300

    feat: create previews page

---
+++ src/pages/CompanyPassAlter/index.tsx
@@ -0,0 +1,39 @@
+import { Button } from '../../components/Button';
+import { Footer } from '../../components/Footer';
+import InputWrapper from '../../components/InputWrapper';
+import Header from '../../components/Portal/Header';
+import { Form, Title } from '../CandidateSettings/style';
+import { Container, Main, Position, Row } from '../styles/CompanyPortalStyles';
+
+export const CompanyPassAlter: React.FC = () => {
+    return (
+        <Container>
+            <Header />
+            <Main>
+                <Title>Alterar Senha</Title>
+                <Row />
+            </Main>
+            <Form style={{ gridTemplateColumns: '1fr', maxWidth: '30%'}}>
+                <InputWrapper>
+                    <label htmlFor="">Senha atual</label>
+                    <input type="password" placeholder='Insira sua senha'/>
+                </InputWrapper>
+                <InputWrapper>
+                    <label htmlFor="">Nova senha</label>
+                    <input type="password" placeholder='Insira sua senha'/>
+                </InputWrapper>
+                <InputWrapper>
+                    <label htmlFor="">Cofirmar nova senha</label>
+                    <input type="password" placeholder='Confirme sua nova senha'/>
+                </InputWrapper>
+                <div>
+                    <Button type="submit">Alterar</Button>
+                </div>
+            </Form>
+            <Position>
+                <Main />
+                <Footer />
+            </Position>
+        </Container>
+    );
+};
